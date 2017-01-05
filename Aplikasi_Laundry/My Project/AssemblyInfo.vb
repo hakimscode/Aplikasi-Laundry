288297a3757685d07d48541db22d3309bc507354
Rawa Tech <techrawa@gmail.com>
2017-01-05 10:22:31 +0700

    Add files via upload

---
+++ Aplikasi_Laundry/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Aplikasi_Laundry")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("SYSTEM-X")> 
+<Assembly: AssemblyProduct("Aplikasi_Laundry")> 
+<Assembly: AssemblyCopyright("Copyright © SYSTEM-X 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3d2a78c6-8bd0-45a5-9ea5-23f235bda2e6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
